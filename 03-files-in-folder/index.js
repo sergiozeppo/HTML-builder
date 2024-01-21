dc41bdd0075e60123755eaeef25f83ef83fb51ad
sergiozeppo <sp1tfire_90@mail.ru>
2024-01-21 12:56:14 +0600

    fix: fix error handling

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,17 +5,21 @@ const secretPath = path.join(__dirname, 'secret-folder');
 
 fs.readdir(secretPath, { withFileTypes: true }, (err, files) => {
   if (err) console.log(err);
-  files.forEach((file) => {
-    if (file.isFile()) {
-      const filePath = path.join(secretPath, file.name);
-      const fileExt = path.extname(filePath).slice(1);
-      const fileName = path.basename(filePath).replace(`.${fileExt}`, '');
-      fs.stat(filePath, (err, stats) => {
-        if (err) console.log(err);
-        console.log(
-          `${fileName} - ${fileExt} - ${(stats.size / 1024).toFixed(3)}Kb`,
-        );
-      });
-    }
-  });
+  else {
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const filePath = path.join(secretPath, file.name);
+        const fileExt = path.extname(filePath).slice(1);
+        const fileName = path.basename(filePath).replace(`.${fileExt}`, '');
+        fs.stat(filePath, (err, stats) => {
+          if (err) console.log(err);
+          else {
+            console.log(
+              `${fileName} - ${fileExt} - ${(stats.size / 1024).toFixed(3)}Kb`,
+            );
+          }
+        });
+      }
+    });
+  }
 });
