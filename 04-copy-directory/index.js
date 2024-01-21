521c2d9c1cd9292d1aec0478c17505420e0d5bc4
sergiozeppo <sp1tfire_90@mail.ru>
2024-01-21 13:25:43 +0600

    feat: solve task 4

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,42 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const pathToFolder = path.join(__dirname, 'files');
+const pathToCopiedFolder = path.join(__dirname, 'files-copy');
+
+function copyDir() {
+  fs.mkdir(pathToCopiedFolder, { recursive: true }, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
+  fs.readdir(pathToCopiedFolder, (err, files) => {
+    if (err) console.log(err);
+    else {
+      files.forEach((file) => {
+        const pathToFile = path.join(pathToCopiedFolder, file);
+        fs.unlink(pathToFile, (err) => {
+          if (err) {
+            return console.log(err);
+          }
+        });
+      });
+    }
+  });
+  fs.readdir(pathToFolder, (err, files) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      files.forEach((file) => {
+        const pathToFile = path.join(pathToFolder, file);
+        const copyFilePath = `${pathToCopiedFolder}/${file}`;
+        fs.copyFile(pathToFile, copyFilePath, (err) => {
+          if (err) {
+            return console.log(err);
+          }
+        });
+      });
+    }
+  });
+}
+copyDir();
