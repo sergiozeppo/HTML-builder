114a773e224e23b2c1ed777c0996b234a9663553
sergiozeppo <sp1tfire_90@mail.ru>
2024-01-24 23:07:11 +0600

    fix: fix creating file after script executing and use another method to fix "includes" behaviour

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,11 +4,14 @@ const process = require('node:process');
 const { stdin, stdout } = process;
 
 const file = path.join(__dirname, 'new.txt');
+fs.writeFile(file, '', { flag: 'a' }, (err) => {
+  if (err) return console.log(err);
+});
 
 stdout.write('Hello, reviewer! Please, enter some text\n');
 stdin.on('data', (data) => {
   const inputData = data.toString();
-  if (inputData.includes('exit')) process.exit();
+  if (inputData.toLowerCase().trim() === 'exit') process.exit();
   fs.writeFile(file, data, { flag: 'a' }, (err) => {
     if (err) return console.log(err);
   });
